dbf5b44db44b8440b03ab34f6d67121e1eaf949d
Diego Andres Olivera Abarca <2024_diego.olivera@iticbcn.cat>
2025-03-14 20:40:42 +0100

    map working with current location

--- js/map.js
+++ js/map.js
@@ -1,8 +1,35 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("map started to load");
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    const map = L.map('map').setView([0, 0], 2);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const { latitude, longitude } = position.coords;
+                map.setView([latitude, longitude], 13);
+                L.marker([latitude, longitude])
+                    .addTo(map)
+                    .bindPopup("Estás aquí")
+                    .openPopup();
+            },
+            (error) => {
+                console.error("Error obteniendo la ubicación: ", error);
+            }
+        );
+    } else{
+        alert("La geolocalización no está soportada en tu navegador");
+    }
+
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords;
+            console.log(`Latitud: ${latitude}, Longitud: ${longitude}`);
+        },
+        (error) => {
+            console.error("Error obteniendo la ubicación: ", error);
+        }
+    );
 });
