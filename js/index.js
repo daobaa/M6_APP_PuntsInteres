8c2fd293240b3256124e752bcc5ee10a5852def2
Diego Andres Olivera Abarca <2024_diego.olivera@iticbcn.cat>
2025-03-14 19:20:33 +0100

    Basic Structure | css and html link

---
+++ js/index.js
@@ -0,0 +1,44 @@
+class PuntInteres {
+    #id;
+    #esManual;
+    pais;
+    ciutat;
+    nom;
+    direccio;
+    tipus;
+    latitud;
+    longitud;
+    puntuacio;
+    static totalTasques;
+
+    constructor(id, esManual) {
+        this.#id = id;
+        this.#esManual = esManual;
+    }
+
+    get id() {
+        return this.#id;
+    }
+    set id(newID) {
+        this.#id = newID; 
+    }
+    get esManual() {
+        return this.#esManual;
+    }
+    set esManual(newManual) {
+        this.#esManual = newManual;
+    }
+
+    static obtenirTotalElements() {
+        
+    }
+}
+class Atraccio extends PuntInteres{
+    horaris;
+    preu;
+    moneda;
+
+    get preuIva() {
+        return 
+    }
+}
