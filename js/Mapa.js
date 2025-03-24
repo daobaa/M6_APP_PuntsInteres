84e5667a1cfab69202b2d50829d0167e13976e33
Diego Andres Olivera Abarca <2024_diego.olivera@iticbcn.cat>
2025-03-24 17:17:15 +0100

    Map under map constructor in class

--- js/Mapa.js
+++ js/Mapa.js
@@ -2,39 +2,33 @@ document.addEventListener("DOMContentLoaded", function(event) {
     class Mapa {
         #map;
 
+        constructor(){
+            this.#map = L.map('map').setView([0, 0], 2);
 
-    }
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(this.#map);
 
-    const map = L.map('map').setView([0, 0], 2);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                const { latitude, longitude } = position.coords;
-                map.setView([latitude, longitude], 13);
-                L.marker([latitude, longitude])
-                    .addTo(map)
-                    .bindPopup("Estás aquí")
-                    .openPopup();
-            },
-            (error) => {
-                console.error("Error obteniendo la ubicación: ", error);
+            if(navigator.geolocation){
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        const { latitude, longitude } = position.coords;
+                        this.#map.setView([latitude, longitude], 13);
+                        L.marker([latitude, longitude])
+                            .addTo(this.#map)
+                            .bindPopup("Estás aquí")
+                            .openPopup();
+                    },
+                    (error) => {
+                        console.error("Error obteniendo la ubicación: ", error);
+                    }
+                );
+            } else{
+                alert("La geolocalización no está soportada en tu navegador");
             }
-        );
-    } else{
-        alert("La geolocalización no está soportada en tu navegador");
+        }
     }
 
-    navigator.geolocation.getCurrentPosition(
-        (position) => {
-            const { latitude, longitude } = position.coords;
-            console.log(`Latitud: ${latitude}, Longitud: ${longitude}`);
-        },
-        (error) => {
-            console.error("Error obteniendo la ubicación: ", error);
-        }
-    );
+    const mapa = new Mapa();
 });
