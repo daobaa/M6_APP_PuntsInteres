ce925e6754439ef1ec709eafd13afb35e6dfa771
Diego Andres Olivera Abarca <2024_diego.olivera@iticbcn.cat>
2025-03-31 08:41:39 +0200

    Moving drag&drop CSV Read content

---
+++ js/Read&Do.js
@@ -0,0 +1,47 @@
+export const TypeSet = new Set();
+
+document.addEventListener("DOMContentLoaded", function() {
+    const dropArea = document.getElementById('attach-files');
+    dropArea.addEventListener('drop', (e) => {
+        e.preventDefault();
+        dropArea.classList.remove('hover');
+    
+        const files = e.dataTransfer.files;
+    
+        if(files[0].name.endsWith(".csv")){
+            const reader = new FileReader();
+            reader.onload = function(event){
+                const fileContent = event.target.result;
+                
+                const rows = fileContent.split('\n');
+                const data = rows.map(row => row.split(','));
+    
+                let espai;
+                let atraccio;
+                let museu;
+    
+                for(let i = 0; i < data.length; i++){
+                    let rowFull = data[i];
+                    if(data[i][3] == "Espai"){
+                        espai = rowFull;
+                        TypeSet.add(data[i][3]);
+                    } else if(data[i][3] == "Atraccio"){
+                        atraccio = rowFull;
+                        TypeSet.add(data[i][3]);
+                    } else if(data[i][3] == "Museu"){
+                        museu = rowFull;
+                        TypeSet.add(data[i][3]);
+                    }
+                }
+    
+                const dataReadyEvent = new CustomEvent('dataReady', {
+                    detail: { espai, atraccio, museu }
+                });
+                document.dispatchEvent(dataReadyEvent);
+            };
+            reader.readAsText(files[0]);
+        } else{
+            alert("El fitxer no és csv");
+        }
+    });
+});
