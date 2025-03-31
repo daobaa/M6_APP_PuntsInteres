e5543e9e1c580c89077e8eb9b74427abe4c4747d
Diego Andres Olivera Abarca <2024_diego.olivera@iticbcn.cat>
2025-03-31 08:40:56 +0200

    Removed Reading and Actions on CSV

--- js/drag&drop.js
+++ js/drag&drop.js
@@ -1,5 +1,3 @@
-export const TypeSet = new Set();
-
 document.addEventListener("DOMContentLoaded", function() {
     const dropArea = document.getElementById('attach-files');
 
@@ -17,48 +15,5 @@ document.addEventListener("DOMContentLoaded", function() {
     
     dropArea.addEventListener('dragleave', () => {
         dropArea.classList.remove('hover');
-    });
-    
-    dropArea.addEventListener('drop', (e) => {
-        e.preventDefault();
-        dropArea.classList.remove('hover');
-
-        const files = e.dataTransfer.files;
-
-        if(files[0].name.endsWith(".csv")){
-            const reader = new FileReader();
-            reader.onload = function(event){
-                const fileContent = event.target.result;
-                
-                const rows = fileContent.split('\n');
-                const data = rows.map(row => row.split(','));
-
-                let espai;
-                let atraccio;
-                let museu;
-
-                for(let i = 0; i < data.length; i++){
-                    let rowFull = data[i];
-                    if(data[i][3] == "Espai"){
-                        espai = rowFull;
-                        TypeSet.add(data[i][3]);
-                    } else if(data[i][3] == "Atraccio"){
-                        atraccio = rowFull;
-                        TypeSet.add(data[i][3]);
-                    } else if(data[i][3] == "Museu"){
-                        museu = rowFull;
-                        TypeSet.add(data[i][3]);
-                    }
-                }
-
-                const dataReadyEvent = new CustomEvent('dataReady', {
-                    detail: { espai, atraccio, museu }
-                });
-                document.dispatchEvent(dataReadyEvent);
-            };
-            reader.readAsText(files[0]);
-        } else{
-            alert("El fitxer no és csv");
-        }
     });
 });
