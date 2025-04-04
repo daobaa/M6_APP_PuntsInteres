a7dc8fa31274b3a68515fd8f4eccf88529cec1cf
Diego Andres Olivera Abarca <2024_diego.olivera@iticbcn.cat>
2025-04-04 19:54:37 +0200

    Order cards base

---
+++ js/SelectOrder.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const orderSelect = document.getElementById("orderChoose");
+    const cardContainer = document.getElementById("body-info");
+
+    orderSelect.addEventListener("change", function(){
+        const selectedOrder = orderSelect.value;
+        
+        if(selectedOrder == "Ascendent"){
+
+        } else if(selectedOrder == "Descendent"){
+
+        }
+    });
+});
